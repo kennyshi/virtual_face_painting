c3d552ad33c19415ab8765f468eb34b6ce6a42f9
Kenny Shi <kenny.shi@gmail.com>
2020-10-08 16:32:12 -0700

    fix body too large

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false })) 
+app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
 app.use(express.json());
 app.use(cookieParser());
 
